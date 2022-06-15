d5aad14fa55e0e7c3ee3048f0b4d882a64271037
atesmen <63913174+atesmen@users.noreply.github.com>
2022-06-15 16:37:42 -0700

    xx

--- week4/day3/cornercoin/index.js
+++ week4/day3/cornercoin/index.js
@@ -10,6 +10,17 @@ class Withdrawal {
   }
 }
 
+class Deposit {
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  commit() {
+    balance += this.amount;
+  }
+}
+
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
@@ -20,5 +31,9 @@ console.log('Transaction 1:', t1);
 t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
+
+t3 = new Deposit(120.0);
+t3.commit();
+console.log('Transaction 3:', t3);
 
 console.log('Balance:', balance);
