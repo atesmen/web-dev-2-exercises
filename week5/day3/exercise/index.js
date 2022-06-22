6323077f251dbe21aecd5d1e23e8d9907eeae8ad
atesmen <63913174+atesmen@users.noreply.github.com>
2022-06-22 16:54:00 -0700

    xx

--- week5/day3/exercise/index.js
+++ week5/day3/exercise/index.js
@@ -1,3 +1,45 @@
 $(() => {
   // code here
+  $.ajax({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    datatype: 'json',
+  }).done((data) =>{
+    console.log('data get ajax', data);
+    $.each(data, (i, post) =>{
+      $('#posts').append(`
+      
+      <div class='post'>
+      <h2>${post.title}</h2>
+      <p>${post.body}</p>
+      </div>
+      `
+      );
+    });
+  });
+
+  $.ajax({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/comments',
+    datatype: 'json',
+  }).done((data) =>{
+    console.log('data get ajax', data);
+    $.each(data, (i, comments) =>{
+      $('#posts').append(`
+      <button id="slideTog">Slide Toggle</button>
+      <div id="comment">
+      <h2>${comments.name}</h2>
+      <p>${comments.body}</p>
+      </div>
+      `
+      );
+    });
+  });
+
+  $('#slideTog').on('click', () => {
+    $('#comment').slideToggle(3000, () => {
+      console.log('slide toggle done');
+    });
+  });
+
 });
